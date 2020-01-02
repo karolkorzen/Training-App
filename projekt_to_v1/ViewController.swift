365a1c2095d7c61300925798cc4da4ed7a4df2f0
Karol <karol.korzen@icloud.com>
2020-01-02 17:07:29 +0100

    Initial Commit

---
+++ projekt_to_v1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projekt_to_v1
+//
+//  Created by Karol Korzeń on 02/01/2020.
+//  Copyright © 2020 Karol Korzeń. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
